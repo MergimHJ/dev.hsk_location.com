00a14d54d6dc743f62d87d022d5366781c9c3faf
MergimHJ <mergimaskaj@hotmail.com>
2025-06-02 19:18:42 +0200

    récupération et correction de la base de donnée, et arrangement des fichiers

--- app/config/db.php
+++ app/config/db.php
@@ -3,8 +3,8 @@ function db() {
     static $pdo = null;
     if ($pdo === null) {
         $host = 'localhost';
-        $db   = 'dev.hsk_location.com';
-        $user = 'dev.hsk_location.com';
+        $db   = 'hsk_location';
+        $user = 'hsk_location';
         $pass = 'sandbox';
         $charset = 'utf8mb4';
 
@@ -20,19 +20,19 @@ function db() {
     return $pdo;
 }
 
-$host = 'localhost';
-$db   = 'dev.hsk_location.com';
-$user = 'dev.hsk_location.com';
-$pass = 'sandbox';
-$charset = 'utf8mb4';
+// $host = 'localhost';
+// $db   = 'dev.hsk_location.com';
+// $user = 'dev.hsk_location.com';
+// $pass = 'sandbox';
+// $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false
-];
+// $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+// $options = [
+//     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//     PDO::ATTR_EMULATE_PREPARES   => false
+// ];
 
-$pdo = new PDO($dsn, $user, $pass, $options);
+// $pdo = new PDO($dsn, $user, $pass, $options);
 
 
